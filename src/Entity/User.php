7fa6f4124683d7566d8df7a9a48fe2c851312735
Mel.R <mel.redureau@gmail.com>
2024-02-12 15:15:06 +0100

    Supprime l'opération custom dans User.php.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,11 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-#[ApiResource(
-    operations: [
-        new GetCollection()
-    ]
-)]
+#[ApiResource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
