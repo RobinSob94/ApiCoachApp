7ede5c3919cfe8508206267a9d1a6df135ed41d0
Mel.R <mel.redureau@gmail.com>
2024-02-15 11:35:48 +0100

    Ajout d'un fonction permettant la récupération des roles user

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -13,12 +13,20 @@ use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class AuthController extends AbstractController
 {
-#[Route("/user/current", name:"get_current_user", methods:["GET"])]
+    #[Route("/user/current", name:"get_current_user", methods:["GET"])]
     public function getCurrentUser(): JsonResponse
     {
         $user = $this->getUser();
-        $userId1 = $user->getId();
-        $userId = $user->getUserIdentifier();
-        return new JsonResponse(['user' => $userId, 'userId' => $userId1]);
+        $userId = $user->getId();
+        $email = $user->getUserIdentifier();
+        return new JsonResponse(['user' => $email, 'userId' => $userId]);
+    }
+
+    #[Route("/user/roles/{id}", name:"get_user_role", methods:["GET"])]
+    public function getUserRole(int $id, UserRepository $userRepository): JsonResponse
+    {
+        $userRole = $userRepository->find($id)->getRoles();
+        return new JsonResponse(['role' => $userRole]);
     }
 }
+
